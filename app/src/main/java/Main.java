33cf7c9563292d59a70f98c513b4bdad8b8ca59e
Ryoma Chinen <e205711@ie.u-ryukyu.ac.jp>
2021-12-14 18:38:37 +0900

    Integer.parseInt()メソッドの例外発生を確認

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,13 +2,8 @@
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
+        String str = "百二十三";
 
-        try {
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerExceptionが発生");
-            System.out.println(e.getMessage());
-        }
+        int value = Integer.parseInt(str);
     }
 }
